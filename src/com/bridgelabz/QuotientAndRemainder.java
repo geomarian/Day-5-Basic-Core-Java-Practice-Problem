e9ea9b3c0d7f464dd0d8ee5ee1c745ea2c39057c
geomarian <geomarian520@gmail.com>
2022-09-08 22:23:38 +0530

    Added new file

---
+++ src/com/bridgelabz/QuotientAndRemainder.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @author ASUS
+ *
+ */
+public class QuotientAndRemainder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int divident = 40;
+		 int divisor = 6;		
+	
+		    int quotient = divident / divisor;
+		    int remainder = divident % divisor;
+
+		    System.out.println("Quotient = " + quotient);
+		    System.out.println("Remainder = " + remainder);
+		  
+	}
+
+}
